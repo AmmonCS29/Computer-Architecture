9589be089f7c3f1e340a8140ac06d08feb372cb9
AmmonMatthews <ammonmatthews@gmail.com>
2020-06-16 14:44:57 -0600

    dynamic file loading done.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,11 @@
 """CPU functionality."""
 
-import sys
+import sys  
+
+HLT = 0b00000001
+LDI = 0b10000010 
+PRN = 0b01000111
+MUL = 0b10100010 
 
 class CPU:
     """Main CPU class."""
@@ -10,6 +15,13 @@ class CPU:
         self.pc = 0 # program counter, index of the current instruction 
         self.reg = [0] * 8 # 8 registers / like variables 
         self.ram = [0] * 256 #ram
+        self.branchtable ={}
+        self.branchtable[LDI] = self.handle_ldi
+        self.branchtable[PRN] = self.handle_prn
+        self.branchtable[MUL] = self.handle_mul
+        
+
+
     def ram_read(self, address ): # accept the address to read and return the value stored 
         return self.ram[address]
 
@@ -18,24 +30,52 @@ class CPU:
         
     def load(self):
         """Load a program into memory."""
-
+        filename = sys.argv[1]
         address = 0
+        with open(filename) as filehandle:
+            for line in filehandle:
+                line = line.split("#")
+                try:
+                    v = int(line[0], 2)
+                except ValueError:
+                    continue
 
+                # self.ram[address] = v
+                self.ram_write(v, address)
+                address += 1
+
+        print(self.ram[:8])
+
+                
+                
+        
+        # for s in program: 
+        #     print("s", s)
+        #     s = s.split(" ") 
+        #     # p2.append(s)
+        #     for x in s:
+        #         p2.append(x)
+
+        # print("p2", p2)
+        # v = program.pop()
+        # print("pop", v)    
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -66,26 +106,56 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
+    
+    def handle_ldi(self):
+        self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
+        self.pc +=3
+        print("ldi")
+    def handle_prn(self):
+        print(self.ram_read(self.pc + 1))
+        self.pc += 2
+        print("prn")
+    def handle_mul(self):
+        # prod = self.reg[self.ram_read(self.pc + 1)] * self.reg[self.ram_read(self.pc + 2)]
+        # self.reg[self.ram_read(self.pc + 1)] = prod
+        # self.pc += 3
+        print("MUL")
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010 #130
-        PRN = 0b01000111 #
-        running = True 
+        # HLT = 0b00000001
+        # LDI = 0b10000010 
+        # PRN = 0b01000111
+        # MUL = 0b10100010 
+        # running = True 
 
-        while running:
-            IR = self.ram_read(self.pc)
-            register_1 = self.ram_read(self.pc + 1)
-            register_2 = self.ram_read(self.pc + 2)
-            if IR == HLT:
-                running = False
-            elif IR == LDI:
-                self.reg[register_1] = register_2
-                self.pc +=3
-            elif IR == PRN:
-                print(self.reg[register_1])
-                self.pc +=2
-            else:
-                print(f"bad input: {bin(IR)}")
-                running = False 
+        # while running:
+        #     IR = self.ram_read(self.pc)
+        #     register_1 = self.ram_read(self.pc + 1)
+        #     register_2 = self.ram_read(self.pc + 2)
+        #     if IR == HLT:
+        #         running = False
+        #         # self.pc += 1
+        #     elif IR == LDI:
+        #         self.reg[register_1] = register_2
+        #         self.pc +=3
+        #     elif IR == PRN:
+        #         print(self.reg[register_1])
+        #         self.pc +=2
+        #     elif IR == MUL:
+        #         prod = self.reg[register_1] * self.reg[register_2]
+        #         # pos = self.pc + 1
+        #         self.reg[register_1] = prod
+        #         # print(prod)
+        #         self.pc += 3
+        #     else:
+        #         print(f"bad input: {bin(IR)}")
+        #         running = False 
+
+        # ir = LDI
+        # self.branchtable[ir]()
+        # ir = PRN
+        # self.branchtable[ir]()
+        # ir = MUL
+        # self.branchtable[ir]()
+
+
